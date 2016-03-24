23dad6f4d550dc017a08f554f579daa317062a42
James Nolan <j.nolan@otherwise.ch>
2016-03-24 11:58:25 +0100

    Created project structures using rebar

---
+++ drum-challenge/src/drum-challenge.erl
@@ -0,0 +1,18 @@
+-module(drum-challenge).
+
+%% drum-challenge: drum-challenge library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
