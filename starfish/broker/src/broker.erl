c1658f0c78a630a18f08edfbc15af0652255076e
James Nolan <j.nolan@otherwise.ch>
2016-06-09 13:04:01 +0200

    works with multiple connexions. Need to find an alternative for fixed 5 bytes reads

--- starfish/broker/src/broker.erl
+++ starfish/broker/src/broker.erl
@@ -1,30 +1,86 @@
+%%
+%% Sequential echo server.
+%%
 -module(broker).
 
-%% broker: broker library's entry point.
-
--export([start/0]).
-
-
 %% API
+-export([start_link/0, stop/1, loop/2]).
 
-start() ->
-    init_wait().
+-include("broker.hrl").
 
-%% Internals
-init_wait() ->
-    {ok, LSock} = gen_tcp:listen(5678, [binary, {packet, 0}, 
-                                        {active, false}]),
-    {ok, Sock} = gen_tcp:accept(LSock),
-    {ok, Bin} = loop(Sock, []),
-    ok = gen_tcp:close(Sock),
-    Bin.
 
-loop(Sock, Bs) ->
-    case gen_tcp:recv(Sock, 0) of
-        {ok, B} ->
-            loop(Sock, [Bs, B]);
-        {error, closed} ->
-            {ok, list_to_binary(Bs)}
+%% ===================================================================
+%% API.
+%% ===================================================================
+
+%% @doc Start the echo server.
+-spec start_link() -> pid().
+start_link() ->
+    %spawn_link(fun() -> server(-1) end). % <= Try this with the dialyzer!
+    spawn_link(fun() -> server(?PORT) end).
+
+%% Stop the echo server.
+-spec stop(Pid) -> stop when
+    Pid :: pid().
+stop(Pid) ->
+    Pid ! stop.
+
+
+%% ===================================================================
+%% Private functions.
+%% ===================================================================
+
+-spec server(Port) -> true when
+    Port :: inet:port_number().
+server(Port) ->
+    % Since we don't close the listen socket and this server is sequential,
+    % subsequent clients will be allowed to connect until the kernel listen
+    % backlog fills up, which is fine.
+    {ok, LSocket} = gen_tcp:listen(Port, [binary, {reuseaddr, true},
+        {active, false}]), % WARNING No backpressure!
+    % Design point: Why spawning another process instead of just calling
+    % `accept/1` directly? We spawn another process to allow the main one to
+    % be always reactive to Erlang messages, despite the fact that
+    % `gen_tcp:accept/1` is blocking in a unnatural way for Erlang (outside a
+    % `receive` block).
+    Acceptor = spawn_link(fun() -> accept(LSocket) end),
+    receive
+        stop ->
+            ?LOG("Server: stopping~n"),
+            % Terminate the acceptor also if it is blocked.
+            exit(Acceptor, stop);
+        Any ->
+            ?LOG("Server: unexpected msg ~p~n", [Any]),
+            exit({server_unexpected_msg, Any})
     end.
 
-%% End of Module.
+-spec accept(LSocket) -> no_return() when
+    LSocket :: inet:socket().
+accept(LSocket) ->
+    {ok, Socket} = gen_tcp:accept(LSocket), % WARNING Blocking!
+    Pid = spawn(?MODULE, loop, [Socket, <<>>]),
+    ?LOG("Pid: ~p~n", [Pid]),
+    accept(LSocket).
+
+-spec loop(Socket, Bin) -> ok when
+	Bin :: erlang:binary(),
+    Socket :: inet:socket().
+loop(Socket, Rest) ->
+	case Rest of
+		% Todo: Variable size instead of 5
+		<<Any:5/binary, "\r\n\r\n", Next/binary>> ->
+			?LOG("~p got a complete message...~p~n", [self(), Any]),
+			gen_tcp:send(Socket, Any),
+			loop(Socket, Next);			
+		_ ->
+			?LOG("~p is waiting for next message...~n", [self()]),
+			case gen_tcp:recv(Socket, 0) of
+				{ok, Bin} ->
+					?LOG("~p got tcp ~p~n", [self(), Bin]),
+					loop(Socket, <<Rest/binary,Bin/binary>>);
+		        Any ->
+					?LOG("~p got unexpected ~p ~n", [self(), Any]),
+		            exit({loop_unexpected_msg, Any})
+			end
+	end.
+
---
+++ starfish/broker/src/broker.hrl
@@ -0,0 +1,4 @@
+-define(PORT, 12345).
+
+-define(LOG(Fmt), io:format(Fmt)).
+-define(LOG(Fmt, Args), io:format(Fmt, Args)).
